757df813523d4dabf4a030e0994ba4ab498241d6
42-djc <118062286+42-djc@users.noreply.github.com>
2023-03-20 20:12:02 +0000

    added 3 questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,58 @@
-// Starter time
-let timer = 60;
+const startButton = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
+const questionsScreen = document.querySelector("#questions");
+const questionTitle = document.querySelector("#question-title");
+const choicesEl = document.querySelector("#choices");
+const timerEl = document.querySelector("#time");
 
-// add time to timer div
-document.getElementById("time").innerHTML = timer;
+let timeLeft = 60; // change this to however long you want the quiz to be in seconds
+let questionIndex = 0;
 
-// countdown 
+startButton.addEventListener("click", function() {
+  startScreen.classList.add("hide");
+  questionsScreen.classList.remove("hide");
+  displayQuestion();
+  startTimer();
+});
 
-setInterval (function(){
-    timer-- 
-    document.getElementById("time").innerHTML = timer;
-},1000);
+function displayQuestion() {
+  let question = questions[questionIndex];
+  questionTitle.textContent = question.question;
+  choicesEl.innerHTML = "";
+  question.choices.forEach(function(choice) {
+    let button = document.createElement("button");
+    button.textContent = choice;
+    button.setAttribute("class", "choice");
+    button.setAttribute("value", choice);
+    button.addEventListener("click", checkAnswer);
+    choicesEl.appendChild(button);
+  });
+}
+
+function checkAnswer(event) {
+  let selectedAnswer = event.target.value;
+  let question = questions[questionIndex];
+  if (selectedAnswer === question.correctAnswer) {
+    // display feedback for correct answer
+  } else {
+    // display feedback for incorrect answer
+    timeLeft -= 10;
+  }
+  questionIndex++;
+  if (questionIndex < questions.length) {
+    displayQuestion();
+  } else {
+    // end the quiz
+  }
+}
+
+function startTimer() {
+  let timer = setInterval(function() {
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      // end the quiz
+    }
+  }, 1000);
+}
---
+++ assets/js/scores.js
@@ -0,0 +1,1 @@
+//var todos = JSON.parse(localStorage.getItem("todos"))
