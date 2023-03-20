534aacfd2017f8a8d72c434b42561d13420db40c
42-djc <118062286+42-djc@users.noreply.github.com>
2023-03-20 20:56:48 +0000

    added DOM elements and event listener on start button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,58 +1,34 @@
+//declare DOM ID and classes
+
 const startButton = document.querySelector("#start");
 const startScreen = document.querySelector("#start-screen");
 const questionsScreen = document.querySelector("#questions");
 const questionTitle = document.querySelector("#question-title");
 const choicesEl = document.querySelector("#choices");
-const timerEl = document.querySelector("#time");
 
-let timeLeft = 60; // change this to however long you want the quiz to be in seconds
-let questionIndex = 0;
 
 startButton.addEventListener("click", function() {
-  startScreen.classList.add("hide");
-  questionsScreen.classList.remove("hide");
-  displayQuestion();
-  startTimer();
-});
-
-function displayQuestion() {
-  let question = questions[questionIndex];
-  questionTitle.textContent = question.question;
-  choicesEl.innerHTML = "";
-  question.choices.forEach(function(choice) {
-    let button = document.createElement("button");
-    button.textContent = choice;
-    button.setAttribute("class", "choice");
-    button.setAttribute("value", choice);
-    button.addEventListener("click", checkAnswer);
-    choicesEl.appendChild(button);
+    startScreen.classList.add("hide");
+    questionsScreen.classList.remove("hide");
+    startTimer();    
   });
-}
 
-function checkAnswer(event) {
-  let selectedAnswer = event.target.value;
-  let question = questions[questionIndex];
-  if (selectedAnswer === question.correctAnswer) {
-    // display feedback for correct answer
-  } else {
-    // display feedback for incorrect answer
-    timeLeft -= 10;
-  }
-  questionIndex++;
-  if (questionIndex < questions.length) {
-    displayQuestion();
-  } else {
-    // end the quiz
-  }
-}
+//clearInterval(intervalId)
 
+// countdown 
+// Set initial timer value
+
+let timer = 60;
+document.getElementById("time").innerHTML = timer;
+
+// Start timer
 function startTimer() {
-  let timer = setInterval(function() {
-    timeLeft--;
-    timerEl.textContent = timeLeft;
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      // end the quiz
+  let intervalId = setInterval(function() {
+    timer--;
+    document.getElementById("time").innerHTML = timer;
+    if (timer <= 0) {
+      clearInterval(intervalId);
     }
   }, 1000);
 }
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,18 @@
 let questions = [
     {
-        question: "Which built-in method combines the text of two strings and returns a new string?",
-        choices: ["append()", "concat()", "attach()", "None of the above"],
-        correctAnswer: "concat()"
+      question: "Which built-in method combines the text of two strings and returns a new string?",
+      choices: ["append()", "concat()", "attach()", "None of the above"],
+      correctAnswer: "concat()"
     },
     {
-        question: "Which of the following function of Array object calls a function for each element in the array?",
-        choices: ["concat()", "every()", "filter()", "forEach()"],
-        correctAnswer: "forEach()"
+      question: "Which of the following function of Array object calls a function for each element in the array?",
+      choices: ["concat()", "every()", "filter()", "forEach()"],
+      correctAnswer: "forEach()"
     },
     {
-        question: "Which of the following is an advantage of using JavaScript?",
-        choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
-        correctAnswer: "All of the above"
+      question: "Which of the following is an advantage of using JavaScript?",
+      choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the them"],
+      correctAnswer: "All of the them"
     }
-];  
+  ];
+  
